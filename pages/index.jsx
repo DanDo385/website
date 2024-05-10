9bd3a5d0b239dc95cb8fda0cd563e4146bc2161d
Daniel Magro <dando385@yahoo.com>
2024-05-10 08:38:07 -0400

    fixed typo on homepage

--- pages/index.jsx
+++ pages/index.jsx
@@ -20,7 +20,7 @@ const Home = () => {
           technology, especially in tokenizing various assets - a theme <br />
           central to my portfolio. I am excited playing a role in <br />
           advancing the decentralization of finance, striving to forge <br />
-          a fairer and more inclusive finacial system in the future.</h2>
+          a fairer and more inclusive financial system in the future.</h2>
                 
         <h2 className="font-poppins text-green-300 text-center text-2xl" style={{ fontFamily: 'poppins, sans-serif' }}></h2>
       <div className="mt-10">
